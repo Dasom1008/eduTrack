8eb5bb73414549ecbfa68a88d76708bdbb994967
Dasom1008 <heaven10087@gmail.com>
2025-04-15 00:04:45 +0900

    화면 tiles 설정 헤더, 푸터, 메인화면 구현

--- edutrack/src/main/java/com/edutrack/EdutrackApplication.java
+++ edutrack/src/main/java/com/edutrack/EdutrackApplication.java
@@ -1,4 +1,4 @@
-package com.edutrack.edutrack;
+package com.edutrack;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ edutrack/src/main/java/com/edutrack/controller/HomeController.java
@@ -0,0 +1,14 @@
+// src/main/java/com/edutrack/controller/HomeController.java
+package com.edutrack.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String main() {
+        return "main"; // templates/main.html 렌더링
+    }
+}
